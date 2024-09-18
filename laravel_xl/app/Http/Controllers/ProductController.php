3884d79f9e96b9fe0f26fc13d4f209f7b9800f66
mehdi <nezoso021@gmail.com>
2024-09-18 17:11:12 +0330

    route
    practice rotues

---
+++ laravel_xl/app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'ok';
+    }
+    public function post_product()
+    {
+        return $number;
+    }
+    public function user_pro(string $user_id,string $comment,string $mobile)
+    {
+        return redirect()->route('category',[45 , 67 , 'string_value']); //$user_id.'___'.$comment.'___'.$mobile;
+    }
+    public function single(string $singlePage,Product $product)
+    {
+        print_r($product);
+    }
+}
--- laravel_xl/routes/web.php
+++ laravel_xl/routes/web.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/product',[ProductController::class,'index']);
+
+Route::get('/product_post/{post}/comments/{comment}',function(string $postId,string $commentId){
+    return $commentId;
+});
+
+Route::post('/user/{id}/comments/{comment}/product/{mobile}',[ProductController::class,'user_pro'])->name('comments');
+
+Route::get('/cat/{cat_id}/attr/{attr_id}/attr_val/{attr_val}',function(Request $request,$cat_id , $attr_id,$attr_val){
+    return $attr_val;
+})->name('category');
+
+Route::prefix('admin')->group(function(){
+    //routes are here
+    Route::get('/product/{admin_id}',[AdminController::class,'index']);
+});
+Route::prefix('front')->group(function(){
+    Route::get('/single_page/{singlePage}',[ProductController::class,'single'])->name('single');
 });
